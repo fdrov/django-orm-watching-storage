8b9a0c7bc396e71d085c8bba4d1eeffe0e15cd84
Fdrv <oldgot@gmail.com>
2021-03-27 14:26:16 +0300

    Fix is_visit_long func, Del strange_person func

--- datacenter/models.py
+++ datacenter/models.py
@@ -47,19 +47,6 @@ def format_duration(duration):
 
 
 def is_visit_long(visit, minutes=60):
-    if visit.leaved_at:
-        duration = (visit.leaved_at - visit.entered_at).total_seconds()
-        return duration > (minutes * 60)
-    else:
-        duration = (django.utils.timezone.localtime() - visit.entered_at).total_seconds()
-        return duration > (minutes * 60)
+    duration = get_duration(visit).total_seconds()
+    return duration > (minutes * 60)
 
-
-def is_person_strange(any_visit):
-    all_person_visits = Visit.objects.filter(passcard__owner_name=any_visit.passcard.owner_name)
-    is_strange = False
-    for visit in all_person_visits:
-        if is_visit_long(visit):
-            is_strange = True
-            break
-    return is_strange
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,7 +1,7 @@
 import django
 from django.shortcuts import render
 
-from datacenter.models import Visit, get_duration, format_duration
+from datacenter.models import Visit, get_duration, format_duration, is_visit_long
 
 
 def storage_information_view(request):
@@ -14,7 +14,7 @@ def storage_information_view(request):
             "who_entered": visit.passcard.owner_name,
             "entered_at": django.utils.timezone.localtime(visit.entered_at),
             "duration": format_duration(get_duration(visit)),
-            "is_strange": True
+            "is_strange": is_visit_long(visit)
         })
 
     context = {
